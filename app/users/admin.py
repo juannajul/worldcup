bc30bc61a1ebe552e5014204cb5e31b03c599c0f
juanNajul <jnajul@marnagarage.com>
2022-11-15 11:48:25 -0400

    new fields in app users admin

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from users.models.users import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'credits', 'date_joined', 'last_login')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
